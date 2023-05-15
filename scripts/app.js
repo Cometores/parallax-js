5767a69807301caefb69ff53940fbb2bf9cc89f6
Cometores <anicometores@gmail.com>
2023-05-15 18:26:50 +0200

    parallax x, y

--- scripts/app.js
+++ scripts/app.js
@@ -4,8 +4,14 @@ let xValue = 0,
     yValue = 0;
 
 window.addEventListener("mousemove", (e) => {
+    /* Coordinates from the center of the screen */
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
-    console.log(xValue, yValue)
+    parallax_el.forEach(el => {
+        let speedx= el.dataset.speedx;
+        let speedy= el.dataset.speedy;
+        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) 
+        translateY(calc(-50% + ${yValue * speedy}px))`;
+    });
 })
