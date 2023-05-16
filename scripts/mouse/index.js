484455d04bc5a069d60595ff632d511c2376eeb1
Cometores <anicometores@gmail.com>
2023-05-16 20:00:06 +0200

    img-object optimization

--- scripts/mouse/index.js
+++ scripts/mouse/index.js
@@ -2,35 +2,51 @@ import parallaxSpeed from './parallaxSpeed.json' assert { type: "json" };
 
 const parallax_el = document.querySelectorAll(".parallax");
 
+/* Mouse coordinates from the center of the screen */
 let xValue = 0,
     yValue = 0;
 
 let rotateDegree = 0;
 
+let imgArray = [];
+
 function update(cursorX) {
-    parallax_el.forEach((el) => {
-        const speeds = parallaxSpeed[el.classList[1]];
-        let speedx= speeds.x;
-        let speedy= speeds.y;
-        let speedz= speeds.z;
-        let rotateSpeed = speeds.rotation;
-
+    imgArray.forEach((img) => {
         /* Taking x-mouse-pos and component's "left" CSS property */
-        let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : - 1;
-        let zValue = (cursorX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+        let inLeft = parseFloat(getComputedStyle(img.el).left) < window.innerWidth / 2 ? 1 : - 1;
+        let zValue = (cursorX - parseFloat(getComputedStyle(img.el).left)) * inLeft * 0.1;
 
-        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) 
-                translateY(calc(-50% + ${yValue * speedy}px)) 
-                rotateY(${rotateDegree * rotateSpeed}deg)
-                perspective(2300px) translateZ(${zValue * speedz}px)`;
+        /* Changing transform CSS property*/
+        img.el.style.transform = `translateX(calc(-50% + ${-xValue * img.speedx}px)) 
+                translateY(calc(-50% + ${yValue * img.speedy}px)) 
+                perspective(2300px) translateZ(${zValue * img.speedz}px)
+                rotateY(${rotateDegree * img.rotateSpeed}deg)`;
     });
 }
 
-/* Centering elements when the page reloads */
-update(0);
+function initialization() {
+    parallax_el.forEach((el) => {
+        const speeds = parallaxSpeed[el.classList[1]];
+        imgArray.push(
+            {
+                el: el,
+                speedx: speeds.x,
+                speedy: speeds.y,
+                speedz: speeds.z,
+                rotateSpeed: speeds.rotation
+            }
+        )
+    });
+
+    /* Centering elements when the page reloads */
+    update(0);
+}
+
+initialization();
 
 window.addEventListener("mousemove", (e) => {
-    /* Coordinates from the center of the screen */
+
+    /* Mouse coordinates from the center of the screen */
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
